3100f22102701c1716252f98afca6c241fe90ce7
vitorbisterso <vitorbisterso@gmail.com>
2026-01-28 20:54:33 -0300

    feat: Add example collection

---
+++ src/api/pix/controllers/pix.ts
@@ -0,0 +1,7 @@
+/**
+ * pix controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::pix.pix');
---
+++ src/api/pix/routes/pix.ts
@@ -0,0 +1,7 @@
+/**
+ * pix router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::pix.pix');
